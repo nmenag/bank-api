47ec2b3b2e1b9ff467cba3c8f66140c99638fb93
nmenag <nmena.garzon@gmail.com>
2021-04-10 20:12:16 -0500

    fix up test enviroment

--- config/test.exs
+++ config/test.exs
@@ -12,6 +12,18 @@ config :bank_api, BankAPI.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
+
+config :bank_api, BankAPI.EventStore,
+  serializer: EventStore.JsonSerializer,
+  username: "postgres",
+  password: "hainover1",
+  database: "bank_api_eventstore_test",
+  hostname: "localhost",
+  pool_size: 1
+
+config :bank_api, :event_store,
+  adapter: Commanded.EventStore.Adapters.InMemory
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :bank_api, BankAPIWeb.Endpoint,
